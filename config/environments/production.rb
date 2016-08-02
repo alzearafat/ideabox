6edadf92e4875af73be9801255c65044d0f12529
Alzea Arafat <alzea.arafat@live.com>
2016-08-02 22:20:09 +0700

    asset precompile heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,8 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "ideabox_#{Rails.env}"
   config.action_mailer.perform_caching = false
-
+  config.serve_static_assets = true
+  config.assets.compile = true
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
